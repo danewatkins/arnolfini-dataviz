1c0b64f3501114f9e4045df784172f6eb18952c4
danewatkins <dane.watkins@gmail.com>
2017-09-24 15:32:24 +0000

    conditional write

--- guage-data/get-data.php
+++ guage-data/get-data.php
@@ -1,6 +1,10 @@
 <?php
 
+echo "hello";
 $homepage = file_get_contents('http://deepdrizzle.net/arnolfini/pi7/guage-data/include.txt');
+echo $homepage;
 $text= '<?php'. PHP_EOL;
 $text.= $homepage;
-file_put_contents("include.php", $text,  LOCK_EX);
+$f=file_put_contents("include.php", $text,  LOCK_EX);
+if($f) echo  "put";
+else echo "not";
